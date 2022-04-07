add925f2e7fb9b30b128ae02822ea769646365df
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-04-07 21:24:19 +0300

    Add a submit event

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -36,4 +36,11 @@ toggleDone (id: number) {
  deleteTodo (id: number) {
    this.todos = this.todos.filter ((v, i) => i !== id);
  }
+
+ addTodo() {
+   this.todos.push({
+     content: this.inputTodo,
+     completed:false
+   })
+ }
 }
