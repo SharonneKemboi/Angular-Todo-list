40d79d9b339e32e768490923f7b7f9843b73afee
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-04-07 19:58:29 +0300

    Add a todo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoComponent } from './components/todo/todo.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent,
+    TodosComponent,
+    
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
